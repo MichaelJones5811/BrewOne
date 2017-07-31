69a30fffc52a7e5e5e374e3fcbdaf55ce5183b10
john sander <jrjackets112@gmail.com>
2017-07-30 23:05:43 -0400

    minor update

--- associations.js
+++ associations.js
@@ -1,11 +1,11 @@
 module.exports = function(db) {
-    //ingredient belongs to recipe belongs to user routes
+    //ingredient belongs to recipe
   db.Ingredient.belongsTo(db.Recipe, { foreignKey: { allowNull: false } });
   db.Recipe.hasMany(db.Ingredient, { onDelete: "cascade" });
-  //Sales belongs to Onhand routes
+  //Sales belongs to Onhand
   db.Sales.belongsTo(db.OnHand, { foreignKey: { allowNull: false } });
   db.OnHand.hasMany(db.Sales, { onDelete: "cascade" });
-  //Team belongs to user routes
+  //Receipt belongs to Onhand
   db.Receipt.belongsTo(db.OnHand, { foreignKey: { allowNull: false } });
   db.OnHand.hasMany(db.Receipt, { onDelete: "cascade" });
 }
