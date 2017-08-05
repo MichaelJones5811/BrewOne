2001e152b4b5b838b49be22f6f4117e13d33c33b
Kirsten Olson <keolson26@gmail.com>
2017-08-05 10:43:31 -0400

    react routing home page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Router, Route } from 'react-router';
+import createHistory from 'history/createHashHistory';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Root from './Root';
+
+import Select from '../src/component/Select';
+import Sales from '../src/component/Sales';
+import InvAdj from '../src/component/InvAdj';
+import Vendor from '../src/component/Vendor';
+import Receipts from '../src/component/Receipts';
+
+const customHistory = createHistory();
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Router history={customHistory}>
+
+   		<Route path='/' component={App}>
+
+    </Route>
+</Router>, document.getElementById('root'))
+
+
+
+
+
